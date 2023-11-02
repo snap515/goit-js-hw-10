e864ccd6b1a65872cef092f312f52bc8c84979d5
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-11-02 20:46:40 +0100

    оптимизирует для переиспользования файлы cat-api.js, убирает всю логику работы с DOM в файл index.js, удаляет axios

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,64 +1,19 @@
-import axios from 'axios';
-axios.defaults.headers.common[
-  'live_sOFtik4WDSjybLGVr2xDDZmFIxxo3QmzgPgMcGLZ8T2Ql2Wbo0kv1tsbDr4uf4Wr'
-];
-import { markupCreator, LoadHideEvents } from './helpers';
-import SlimSelect from 'slim-select';
-
 const URL = `https://api.thecatapi.com/v1`;
-const loadHideEvents = new LoadHideEvents();
 
 export function fetchBreeds() {
-  loadHideEvents.showSelectLoader();
-
-  return fetch(`${URL}/breeds`)
-    .then(response => {
-      if (!response.ok) {
-        loadHideEvents.hideSelectLoader();
-        loadHideEvents.showFetchMistake();
-        throw new Error(response.status);
-      }
-
-      return response.json();
-    })
-    .then(data => {
-      const options = data.reduce((option, { name, reference_image_id }) => {
-        option.push({
-          text: name,
-          value: reference_image_id,
-        });
-        return option;
-      }, []);
-      new SlimSelect({
-        select: '#breedSelect',
-        data: options,
-      });
-
-      loadHideEvents.hideSelectLoader();
-    })
-    .catch(error => console.log(error));
+  return fetch(`${URL}/breeds`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
 export function fetchCatByBreed(breedId) {
-  loadHideEvents.showOptionLoading();
-  return fetch(`${URL}/images/${breedId}`)
-    .then(response => {
-      if (!response.ok) {
-        loadHideEvents.hideSelectLoader();
-        loadHideEvents.hideOptionLoading();
-        loadHideEvents.showFetchMistake();
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(({ url, breeds }) => {
-      const catInfoEl = document.querySelector('.cat-info');
-      loadHideEvents.hideOptionLoading();
-      catInfoEl.innerHTML = markupCreator(
-        url,
-        breeds[0].name,
-        breeds[0].description,
-        breeds[0].temperament
-      );
-    });
+  return fetch(`${URL}/images/${breedId}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,59 @@
-import axios from 'axios';
-axios.defaults.headers.common[
-  'live_sOFtik4WDSjybLGVr2xDDZmFIxxo3QmzgPgMcGLZ8T2Ql2Wbo0kv1tsbDr4uf4Wr'
-];
-
+// import axios from 'axios';
+// axios.defaults.headers.common[
+//   'live_sOFtik4WDSjybLGVr2xDDZmFIxxo3QmzgPgMcGLZ8T2Ql2Wbo0kv1tsbDr4uf4Wr'
+// ];
+import SlimSelect from 'slim-select';
+import { markupCreator, LoadHideEvents } from './helpers';
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
 
-fetchBreeds();
+const loadHideEvents = new LoadHideEvents();
 
-const selectEl = document.querySelector('.breed-select');
-
-selectEl.addEventListener('change', onSelectChange);
+loadHideEvents.showSelectLoader();
+fetchBreeds()
+  .then(data => {
+    const options = data.reduce((option, { name, reference_image_id }) => {
+      option.push({
+        text: name,
+        value: reference_image_id,
+      });
+      return option;
+    }, []);
+    new SlimSelect({
+      select: '#breedSelect',
+      data: options,
+    });
+  })
+  .catch(error => {
+    loadHideEvents.hideSelectLoader();
+    loadHideEvents.showFetchMistake();
+    console.dir(error);
+  })
+  .finally(() => {
+    loadHideEvents.hideSelectLoader();
+    const selectEl = document.querySelector('.breed-select');
+    selectEl.addEventListener('change', onSelectChange);
+  });
 
 function onSelectChange(e) {
+  loadHideEvents.showOptionLoading();
   const breedId = e.target.value;
-  fetchCatByBreed(breedId);
+  fetchCatByBreed(breedId)
+    .then(({ url, breeds }) => {
+      const catInfoEl = document.querySelector('.cat-info');
+      catInfoEl.innerHTML = markupCreator(
+        url,
+        breeds[0].name,
+        breeds[0].description,
+        breeds[0].temperament
+      );
+    })
+    .catch(error => {
+      loadHideEvents.hideOptionLoading();
+      loadHideEvents.showFetchMistake();
+      console.log(error);
+    })
+    .finally(() => {
+      loadHideEvents.hideOptionLoading();
+    });
 }
